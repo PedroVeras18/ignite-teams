cabf876afb57020f72ac108aaf4fa2db16548ea1
Pedro Veras <veraspedro1608@gmail.com>
2023-08-13 17:18:38 -0300

    feat: CSS-in-JS: Criando Componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 import { Container } from './styles';
 
@@ -7,9 +8,14 @@ export function Groups() {
   return (
     <Container>
       <Header />
+
       <Highlight
         title="Turmas"
         subtitle="jogue com a sua turma"
+      />
+
+      <GroupCard
+        title='Galera do Ignite'
       />
     </Container>
   );
